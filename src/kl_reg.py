01c13f6acab5a1477f6cbb0aa57551ca1aba2787
Will Merrill <willm@allenai.org>
2021-08-24 10:42:21 -0700

    debug flatten

--- src/kl_reg.py
+++ src/kl_reg.py
@@ -25,8 +25,8 @@ class KlSatReg:
             sat_mask = (probs > max_prob * self.tol)
             counts = sat_mask.sum(dim=-1).unsqueeze(dim=-1)
             sat_probs = sat_mask.float() / counts
-        probs = probs.flatten(start_dim=1)
-        sat_probs = sat_probs.flatten(start_dim=1)
+        probs = probs.flatten(end_dim=-1)
+        sat_probs = sat_probs.flatten(end_dim=-1)
         loss = self.loss(probs.log(), sat_probs)
         assert loss > 0
         return loss
